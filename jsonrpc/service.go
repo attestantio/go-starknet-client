d1bee4d31b9734bf455f2ecc838391d92d74c98b
Jim McDonald <Jim@mcdee.net>
2024-11-20 15:47:08 +0000

    Update version.

--- jsonrpc/service.go
+++ jsonrpc/service.go
@@ -98,7 +98,7 @@ func New(ctx context.Context, params ...Parameter) (*Service, error) {
 	log.Trace().Stringer("address", address).Str("web_socket_address", webSocketAddress).Msg("Addresses configured")
 
 	extraHeaders := map[string]string{
-		"User-Agent": "go-starknet-client/0.1.0",
+		"User-Agent": "go-starknet-client/0.1.7",
 	}
 
 	rpcClient := jsonrpc.NewClientWithOpts(address.String(), &jsonrpc.RPCClientOpts{
